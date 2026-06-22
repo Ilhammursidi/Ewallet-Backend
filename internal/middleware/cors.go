c655ef1235034735435b31779eb1fd2cbf6dc320
ilhammursidi <imursidi37@gmail.com>
2026-06-22 15:49:04 +0700

    feat: add localhost:81 to allowed origin

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -9,17 +9,19 @@ import (
 )
 
 func CORSMiddleware(ctx *gin.Context) {
-	allowedOrigin := []string{"http://localhost:81"}
+	allowedOrigin := []string{"http://localhost:5173", "http://localhost:8081", "http://localhost:81"}
 	currentOrigin := ctx.GetHeader("Origin")
 	if slices.Contains(allowedOrigin, currentOrigin) {
 		ctx.Header("Access-Control-Allow-Origin", currentOrigin)
 	}
 
-	allowedHeaders := []string{"Content-Type", "X-Koda-X", "Authorization"}
+	allowedHeaders := []string{"Content-Type", "X-koda-X", "Authorization", "Origin", "Accept"}
 	ctx.Header("Access-Control-Allow-Headers", strings.Join(allowedHeaders, ", "))
 
 	allowedMethods := []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodPatch, http.MethodOptions}
 	ctx.Header("Access-Control-Allow-Methods", strings.Join(allowedMethods, ", "))
+
+	ctx.Header("Access-Control-Allow-Credentials", "true")
 
 	if ctx.Request.Method == http.MethodOptions {
 		ctx.AbortWithStatus(http.StatusNoContent)
